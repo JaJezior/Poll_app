1764f98e5201b301d1216312e455ec1bc659a930
JaJezior <polocrafter@gmail.com>
2020-04-27 21:10:55 +0200

    Failed adding deserialization logic

---
+++ Poll_app.Business.Logic/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Poll_app;
+
+
+namespace Poll_app.Business.Logic
+{
+    public class DataService
+    {
+        public object JsonConvert { get; private set; }
+
+        public void Serialize()
+        {
+            //Zapis do pliku
+        }
+        public List<Question> Deserialize(string filePath)
+        {
+
+            //Wczytywanie z pliku
+            var jsonData = File.ReadAllText(filePath);
+            var loadedQuestions = JsonConvert.DeserializeObject<List<Question>>(jsonData);
+            return loadedQuestions;
+        }
+    }
+}
--- Poll_app/Pollster.cs
+++ Poll_app/Pollster.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Dynamic;
+using Newtonsoft.Json;
+using Poll_app.Business.Logic;
 
-namespace Poll_app
+namespace Pollster
 {
-    class Program
+    class Pollster
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            new Pollster().Run();        
+        }
+
+        private void Run()
+        {
+            Console.WriteLine("Uruchomiono program Pollster CodeMentos");
+            LoadQuestions();
+        }
+        string input;
+
+        private readonly DataService dataService = new DataService();
+
+        private void LoadQuestions()
+        {
+            Console.WriteLine("Aby wczytać pytania podaj ścieżkę pliku *.json: (np. questions.json");
+            input = Console.ReadLine();
+            dataService.Deserialize(input);
+
         }
     }
 }
