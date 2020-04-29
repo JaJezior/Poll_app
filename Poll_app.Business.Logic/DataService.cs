11214839064360479f7c117a93b8f81fe99c1c4a
JaJezior <polocrafter@gmail.com>
2020-04-29 16:49:58 +0200

    Finished AskQuestion method

--- Poll_app.Business.Logic/DataService.cs
+++ Poll_app.Business.Logic/DataService.cs
@@ -7,7 +7,7 @@ using Poll_app;
 
 
 namespace Poll_app.Business.Logic
-{ 
+{
     public class DataService
     {
         public void Serialize(List<Question> loadedQuestions, string filePath)
@@ -22,11 +22,120 @@ namespace Poll_app.Business.Logic
         {
 
             //Wczytywanie z pliku
-            //List<Question> loadedQuestions = new List<Question>();
+            List<Question> loadedQuestions = new List<Question>();
+
             var jsonData = File.ReadAllText(filePath);
-            var loadedQuestions = JsonConvert.DeserializeObject<List<Question>>(jsonData);
+            loadedQuestions = JsonConvert.DeserializeObject<List<Question>>(jsonData);
             return loadedQuestions;
+
+
+        }
+        public List<Question> AskQuestions(List<Question> loadedQuestions)
+        {
+            if (loadedQuestions is null)
+            {
+                throw new ArgumentNullException(nameof(loadedQuestions));
             }
+
+            for (int i = 0; i < loadedQuestions.Count; i++)
+            {
+                
+                Console.WriteLine($"{loadedQuestions[i].QuestionText}");
+
+                for (int j = 0; j < loadedQuestions[i].possibleAnswers.Count; j++)
+                {
+                    Console.WriteLine($"{loadedQuestions[i].possibleAnswers[j].AnswerIndex}. {loadedQuestions[i].possibleAnswers[j].AnswerText}");
+                }
+
+                bool isAnswerDone = false;
+
+                while (isAnswerDone == false)
+                {
+                    try
+                    {
+                        Console.WriteLine("Wybierz numer odpowiedzi:");
+                        int usersAnswer = int.Parse(Console.ReadLine());
+
+                        if(loadedQuestions[i].possibleAnswers.Exists(x => x.AnswerIndex == usersAnswer))
+                        { 
+
+                        foreach (Answer ans in loadedQuestions[i].possibleAnswers)
+                        {
+                            if (usersAnswer == ans.AnswerIndex)
+                            {
+                                ans.AnswerCount++;
+                                Console.WriteLine($"Zapisano. Twoja odpowiedź to: {usersAnswer}.");
+                                isAnswerDone = true;
+                            }
+                        }
+                        }
+                        if (!loadedQuestions[i].possibleAnswers.Exists(x => x.AnswerIndex == usersAnswer))
+                        {
+                            Console.WriteLine($"Odpowiedź '{usersAnswer}' nie istnieje.");
+                        }
+
+                    }
+                    catch (ArgumentException argexception)
+                    {
+                        Console.WriteLine(argexception.Message);
+                    }
+                    catch (FormatException formatexception)
+                    {
+                        Console.WriteLine(formatexception.Message);
+                    }
+                }
+
+                
+
+            }
+            Console.WriteLine("Nie ma więcej pytań.");
+            return loadedQuestions;
+
+
+        }
+        public List<Question> LoadQuestions()
+        {
+            var input = "";
+            bool isLoadingDone = false;
+
+            while (isLoadingDone == false)
+            {
+
+                Console.WriteLine("Aby wczytać pytania podaj ścieżkę pliku *.json: (np. questions.json).");
+                input = Console.ReadLine();
+
+                try
+                {
+                    Deserialize(input);
+                    Console.WriteLine($"Pytania z pliku {input} zostały wczytane.");
+                    isLoadingDone = true;
+                }
+                catch (ArgumentException ArgException)
+                {
+                    Console.WriteLine(ArgException.Message);
+
+                }
+                catch (FormatException FormatException)
+                {
+                    Console.WriteLine(FormatException.Message);
+                }
+                catch (System.IO.FileNotFoundException FileNFException)
+                {
+                    Console.WriteLine(FileNFException.Message);
+                }
+
+            }
+            return Deserialize(input);
+        }
+
+        public void SaveAnswers(List<Question> loadedQuestions)
+        {
+            if (loadedQuestions is null)
+            {
+                throw new ArgumentNullException(nameof(loadedQuestions));
+            }
+
         }
     }
+}
 
--- Poll_app/Pollster.cs
+++ Poll_app/Pollster.cs
@@ -6,6 +6,7 @@ namespace Pollster
 {
     class Pollster
     {
+        public DataService dataService = new DataService();
         static void Main()
         {
             //InitData();
@@ -23,7 +24,7 @@ namespace Pollster
             Console.WriteLine("Uruchomiono program Pollster CodeMentos");
 
             //LoadQuestions();
-            AskQuestions(LoadQuestions());
+            dataService.AskQuestions(dataService.LoadQuestions());
 
 
 
@@ -31,7 +32,7 @@ namespace Pollster
 
 
 
-        public DataService dataService = new DataService();
+        
 
 
         private static void InitData()
@@ -82,77 +83,9 @@ namespace Pollster
             var _dataService = new DataService();
             _dataService.Serialize(_list, "questions.json");
         }
-        private List<Question> LoadQuestions()
-        {
+        
 
-
-
-            Console.WriteLine("Aby wczytać pytania podaj ścieżkę pliku *.json: (np. questions.json).");
-
-            var input = Console.ReadLine();
-
-
-
-            try
-            {
-                dataService.Deserialize(input);
-                Console.WriteLine($"Pytania z pliku {input} zostały wczytane.");
-
-            }
-            catch (ArgumentException ArgException)
-            {
-                Console.WriteLine(ArgException.Message);
-
-            }
-            catch (FormatException FormatException)
-            {
-                Console.WriteLine(FormatException.Message);
-            }
-            catch (System.IO.FileNotFoundException FileNFException)
-            {
-                Console.WriteLine(FileNFException.Message);
-            }
-            return dataService.Deserialize(input);
-
-
-
-
-
-        }
-
-        private void AskQuestions(List<Question> loadedQuestions)
-        {
-            for (int i = 0; i < loadedQuestions.Count; i++)
-            {
-                Question q = loadedQuestions[i];
-                Console.WriteLine($"{loadedQuestions[i].QuestionText}");
-
-                for (int j = 0; j < loadedQuestions[i].possibleAnswers.Count; j++)
-                {
-                    Console.WriteLine($"{loadedQuestions[i].possibleAnswers[j].AnswerIndex}. {loadedQuestions[i].possibleAnswers[j].AnswerText}");
-                }
-
-                Console.WriteLine("Wybierz numer odpowiedzi:");
-
-                try
-                {
-                    int usersAnswer = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Zapisano. Twoja odpowiedź to: {usersAnswer}.");
-
-                }
-                catch (ArgumentException argexception)
-                {
-                    System.Console.WriteLine(argexception.Message);
-                }
-                catch (FormatException formatexception)
-                {
-                    System.Console.WriteLine(formatexception.Message);
-                }
-                
-            }
-            
-            
-        }
+        
     }
 }
 
