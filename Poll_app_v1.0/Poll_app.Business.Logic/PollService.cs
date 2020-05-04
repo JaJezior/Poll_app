757448f141d42108fccee419dca82a3ca7813700
JaJezior <polocrafter@gmail.com>
2020-05-04 13:15:40 +0200

    CodeRefactoring, added .gitignore

---
+++ Poll_app_v1.0/Poll_app.Business.Logic/PollService.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poll_app.Business.Logic
+{
+    public class PollService
+    {
+        public List<Question> AskQuestions(List<Question> loadedQuestions)
+        {
+            if (loadedQuestions is null)
+            {
+                throw new ArgumentNullException(nameof(loadedQuestions));
+            }
+
+            for (int i = 0; i < loadedQuestions.Count; i++)
+            {
+
+                Console.WriteLine($"{loadedQuestions[i].QuestionText}");
+
+                for (int j = 0; j < loadedQuestions[i].possibleAnswers.Count; j++)
+                {
+                    Console.WriteLine($"{loadedQuestions[i].possibleAnswers[j].AnswerIndex}. {loadedQuestions[i].possibleAnswers[j].AnswerText}");
+                }
+
+                bool isAnswerDone = false;
+
+                while (isAnswerDone == false)
+                {
+                    try
+                    {
+                        Console.WriteLine("Wybierz numer odpowiedzi:");
+                        int usersAnswer = int.Parse(Console.ReadLine());
+
+                        if (loadedQuestions[i].possibleAnswers.Exists(x => x.AnswerIndex == usersAnswer))
+                        {
+
+                            foreach (Answer ans in loadedQuestions[i].possibleAnswers)
+                            {
+                                if (usersAnswer == ans.AnswerIndex)
+                                {
+                                    ans.AnswerCount++;
+                                    Console.WriteLine($"Zapisano. Twoja odpowiedź to: {usersAnswer}.");
+                                    isAnswerDone = true;
+                                }
+                            }
+                            loadedQuestions[i].QuestionCount++;
+                        }
+                        if (!loadedQuestions[i].possibleAnswers.Exists(x => x.AnswerIndex == usersAnswer))
+                        {
+                            Console.WriteLine($"Odpowiedź '{usersAnswer}' nie istnieje.");
+                        }
+
+                    }
+                    catch (ArgumentException argexception)
+                    {
+                        Console.WriteLine(argexception.Message);
+                    }
+                    catch (FormatException formatexception)
+                    {
+                        Console.WriteLine(formatexception.Message);
+                    }
+                }
+
+
+
+            }
+            Console.WriteLine("Nie ma więcej pytań.");
+            return loadedQuestions;
+
+
+        }
+        public List<Question> LoadQuestions()
+        {
+            var input = "";
+            var dataService = new DataService();
+            bool isLoadingDone = false;
+
+            while (isLoadingDone == false)
+            {
+
+                Console.WriteLine("Aby wczytać pytania podaj ścieżkę pliku *.json: (np. questions.json).");
+                input = Console.ReadLine();
+
+                try
+                {
+                    dataService.Deserialize(input);
+                    Console.WriteLine($"Pytania z pliku {input} zostały wczytane.");
+                    isLoadingDone = true;
+                }
+                catch (ArgumentException ArgException)
+                {
+                    Console.WriteLine(ArgException.Message);
+
+                }
+                catch (FormatException FormatException)
+                {
+                    Console.WriteLine(FormatException.Message);
+                }
+                catch (System.IO.FileNotFoundException FileNFException)
+                {
+                    Console.WriteLine(FileNFException.Message);
+                }
+
+            }
+            return dataService.Deserialize(input);
+        }
+        public void SaveAnswers(List<Question> loadedQuestions)
+        {
+            if (loadedQuestions is null)
+            {
+                throw new ArgumentNullException(nameof(loadedQuestions));
+            }
+            var dataService = new DataService();
+            bool isSavingDone = false;
+
+            while (isSavingDone == false)
+            {
+
+                Console.WriteLine("Aby zapisać odpowiedzi podaj docelową ścieżkę pliku *.json: (np. 'answers').");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    dataService.Serialize(loadedQuestions, input);
+                    Console.WriteLine($"Plik {input} został zapisany.");
+                    isSavingDone = true;
+                    Console.ReadKey();
+                }
+                catch (ArgumentException ArgException)
+                {
+                    Console.WriteLine(ArgException.Message);
+
+                }
+                catch (FormatException FormatException)
+                {
+                    Console.WriteLine(FormatException.Message);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Poll_app_v1.0/Poll_app.Business.Logic/ReportService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Poll_app.Business.Logic
+{
+    public class ReportService
+    {
+        public StringBuilder GenerateReport(List<Question> loadedQuestions)
+        {
+            if (loadedQuestions is null)
+            {
+                throw new ArgumentNullException(nameof(loadedQuestions));
+            }
+            StringBuilder reportText = new StringBuilder("");
+            reportText.Append("Raport");
+            ReportContentGenerator(loadedQuestions, reportText);
+            Console.WriteLine(reportText);
+            return reportText;
+        }
+
+        private static void ReportContentGenerator(List<Question> loadedQuestions, StringBuilder reportText)
+        {
+            for (int i = 0; i < loadedQuestions.Count; i++)
+            {
+
+                reportText.Append($"{loadedQuestions[i].QuestionText}");
+                reportText.AppendLine();
+                reportText.Append($"To pytanie zadano następującą liczbę razy: {loadedQuestions[i].QuestionCount}");
+                reportText.AppendLine();
+
+                for (int j = 0; j < loadedQuestions[i].possibleAnswers.Count; j++)
+                {
+                    reportText.Append($"{loadedQuestions[i].possibleAnswers[j].AnswerIndex}. {loadedQuestions[i].possibleAnswers[j].AnswerText}");
+                    reportText.AppendLine();
+                    reportText.Append($"Tę odpowiedź wybrano {loadedQuestions[i].possibleAnswers[j].AnswerCount} raz(-y) na {loadedQuestions[i].QuestionCount} możliwych");
+                    reportText.AppendLine();
+
+                    //Pasek procentowy
+                    StringBuilder percentageBar = PercentageBarBuilder(loadedQuestions, i, j);
+                    reportText.Append(percentageBar);
+                    reportText.AppendLine();
+                }
+            }
+        }
+
+        private static StringBuilder PercentageBarBuilder(List<Question> loadedQuestions, int i, int j)
+        {
+            StringBuilder percentageBar = new StringBuilder("[");
+
+            double thisAnswerPercentage = 100 * loadedQuestions[i].possibleAnswers[j].AnswerCount / loadedQuestions[i].QuestionCount;
+            thisAnswerPercentage = Math.Round(thisAnswerPercentage, 0);
+            for (int x = 1; x <= 100; x++)
+            {
+                if (thisAnswerPercentage >= x)
+                {
+                    percentageBar.Append("█");
+                }
+                if (thisAnswerPercentage < x)
+                {
+                    percentageBar.Append("_");
+                }
+
+            }
+            percentageBar.Append("]");
+            return percentageBar;
+        }
+
+        public void SaveReportToFile(StringBuilder reportText)
+        {
+            Console.WriteLine("Podaj ścieżkę gdzie chcesz zapisać plik:");
+            string input = Console.ReadLine();
+            input += ".txt";
+            File.WriteAllText(input, reportText.ToString());
+
+            Console.WriteLine($"Zakończono generowanie raportu. Zapisano {input}.");
+        }
+    }
+}
